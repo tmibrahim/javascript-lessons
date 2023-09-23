e8d5e790b02144e36a9ae5d2c93d7deaea96d597
tmibrahim <ibrahim-temur@outlook.com>
2023-09-23 22:17:05 +0300

    8 And 9 works added.

--- 8.Dialog Box/app.js
+++ 8.Dialog Box/app.js
@@ -18,4 +18,22 @@ let age = prompt("Enter your Age: " );
 
 console.log("Your Name is : " + name1);
 console.log("Your Age is : " + age);
-console.log(tyoeof name1);
+console.log(typeof name1);
+
+// console.log(window)
+// alert("How are you")
+
+//prompt(); Kullanıcıdan değer alabiliriz.
+
+// let name1 = prompt("Please enter your Name : ");
+// let age   = prompt("Please enter your Age : ");
+
+// console.log("Your name is :" + name1);
+// console.log("Your Age is :" + age);
+
+// console.log(typeof name1);
+// console.log(typeof age);
+
+
+// let result = confirm("Are you sure you want to exit");
+// console.log(result);
---
+++ 9.Kosullar/app.js
@@ -0,0 +1,61 @@
+//KOŞUL YAPILARI
+
+// let note = 35;
+
+// if(note>50){
+//     console.log("You pass, your note is: " + note);
+// }
+// else{
+//     console.log("You did not pass, your note is: " + note);
+// }
+// let age = Number(prompt("Enter your age :"));
+// console.log(typeof age);
+
+// let money = Number(prompt("Your Budged : "));
+
+// if(age>=18 && money>=3000){
+//     alert("You may join to the driver licence exam");
+// }else{
+//     alert("You may not to the driver license exam");
+// }
+
+// Example
+
+//FINDING AVERAGE OF THE LESSON
+
+// vize1 %30, vize2 %30, final %40
+
+// let vize1 = Number(prompt("Enter Vize 1 :"));
+// let vize2 = Number(prompt("Enter Vize 2 :"));
+// let final = Number(prompt("Enter Final :"));
+
+// let ortalama = (vize1*0.3) + (vize2*0.3) + (final*0.40);
+
+// if(ortalama>=60){
+//     alert("You pass the class :" + ortalama);
+//     console.log("You pass the class");
+// }else{
+//     alert("You did not pass :" + ortalama);
+//     console.log("You did not pass");
+// }
+
+//ELSE IF
+
+// example
+
+//Select a proper number between 1 to 3
+
+let selectedNumber = Number(prompt("Enter a number between 1 to 3 :"));
+
+if(selectedNumber == 1){
+    alert("Your selected number is "+ selectedNumber +" .!");
+}
+else if(selectedNumber == 2){
+    alert("Your selected number is "+ selectedNumber +" .!");
+}
+else if(selectedNumber == 3){
+    alert("Your selected number is "+ selectedNumber +" .!");
+}
+else{
+    alert("Please enter aproper number !");
+}
